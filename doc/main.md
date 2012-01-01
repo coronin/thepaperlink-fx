d992de5175e6cd3795ca85d2dc4eab65cad24c8b
Liang Cai <coronin+alien@gmail.com>
2012-01-01 11:28:44 -0800

    bath

--- doc/main.md
+++ doc/main.md
@@ -1,2 +1,14 @@
-The main module is a program that creates a widget.  When a user clicks on
-the widget, the program loads the mozilla.org website in a new tab.
+the Paper Link for PubMed
+--------
+In addition to existing PubMed search results, this extension will add direct PDF link; display F1000 scores, Impact Factor, Peaks and much more! It is built based on publicly available information from NLM / NCBI / NIH, and other sources. 
+
+Detailed enhancement includes:
+ * Impact Factor of the Journal
+ * Direct link to its PDF file
+ * External pages related to the article
+ * Faculty 1000 score and link to its comment page
+ * "Peaks", which is based on Impact Factor (the playground for "Peaks" is at http://2.thepaperlink.com/static/peaks.html)
+ * Email PDF file to yourself
+ * Extract PMID on any webpage
+
+This extension is built using API from http://www.pubmeder.com, http://www.thepaperlink.com and PubMed eUtils.
--- test/test-main.js
+++ test/test-main.js
@@ -1,32 +1,42 @@
-const main = require("main");
+const main = require('main');
 
 exports.test_test_run = function(test) {
-  test.pass("Unit test running!");
+  test.pass('Unit test running!');
 };
 
 exports.test_id = function(test) {
-  test.assert(require("self").id.length > 0);
+  test.assert(require('self').id.length > 0);
 };
 
-exports.test_url = function(test) {
-  require("request").Request({
-    url: "http://www.mozilla.org/",
+exports.test_nonPubMed = function(test) {
+  require('request').Request({
+    url: 'http://www.thepaperlink.com',
     onComplete: function(response) {
-      test.assertEqual(response.statusText, "OK");
+      test.assertEqual(response.statusText, 'OK');
       test.done();
     }
   }).get();
   test.waitUntilDone(20000);
 };
 
-exports.test_open_tab = function(test) {
-  const tabs = require("tabs");
-  tabs.open({
-    url: "http://www.mozilla.org/",
-    onReady: function(tab) {
-      test.assertEqual(tab.url, "http://www.mozilla.org/");
+exports.test_PubMed1 = function(test) {
+  require('request').Request({
+    url: 'http://www.ncbi.nlm.nih.gov/pubmed?term=coronin',
+    onComplete: function(response) {
+      test.assertEqual(response.statusText, 'OK');
       test.done();
     }
-  });
+  }).get();
+  test.waitUntilDone(20000);
+};
+
+exports.test_PubMed2 = function(test) {
+  require('request').Request({
+    url: 'http://www.ncbi.nlm.nih.gov/pubmed/18775315',
+    onComplete: function(response) {
+      test.assertEqual(response.statusText, 'OK');
+      test.done();
+    }
+  }).get();
   test.waitUntilDone(20000);
 };
